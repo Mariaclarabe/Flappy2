54efffe72319ae122f0b6ef1a003a7714ea8a25e
mariaclaraalves <99183245+Mariaclarabe@users.noreply.github.com>
2024-06-10 11:40:05 -0300

    salvo

--- Assets/Scripts/Obstaculo.cs
+++ Assets/Scripts/Obstaculo.cs
@@ -2,24 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Obstaculo : MonoBehaviour {
+public class Obstaculo : MonoBehaviour 
+{
     [SerializeField]
     private float velocidade = 0.6f;
+
     [SerializeField]
     private float variacaoDaPosicaoY;
+    private Vector3 posicaoPassaro;
+    private UIcontroler controladorUI;
+
+    private bool pontuei;
 
     private void Awake()
     {
         this.transform.Translate(Vector3.up * Random.Range(-variacaoDaPosicaoY, variacaoDaPosicaoY));
     }
 
-    private void Update () 
+    private void Start(){
+        this.posicaoPassaro = GameObject.FindObjectOfType<Bird>().transform.position;
+        this.controladorUI = GameObject.FindObjectOfType<UIcontroler>();
+    }
+
+    //Uptade is called one per frame 
+    void Update () 
     {
         this.transform.Translate(Vector3.left * this.velocidade * Time.deltaTime);
+        if(!this.pontuei && this.transform.position.x < posicaoPassaro.x){
+            this.controladorUI.adicionarPontos();
+            this.pontuei = true; 
+        }
        
 	}
 
-    private void OnTriggerEnter2D(Collider2D outro)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         this.Destruir();
     }
